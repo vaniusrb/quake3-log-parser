d5d80d9cc2ce5a352a4270c4651f1de1f7d51616
vaniusrb <vanius@gmail.com>
2024-02-20 21:54:28 -0300

    wip: log parser

---
+++ src/README.md
@@ -0,0 +1,7 @@
+
+
+fast read and parser file:
+https://github.com/mtb0x1/1brc
+
+test url:
+https://gist.github.com/cloudwalk-tests/704a555a0fe475ae0284ad9088e203f1
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
+use ahash::HashMap;
+use memmap2::Mmap;
+use mimalloc::MiMalloc;
+use rayon::{iter::ParallelIterator, slice::*};
+use std::{env::args_os, fs::File, path::Path};
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
+const INIT_GAME: &[u8] = b"InitGame:";
+
+enum Kill {
+    Killer(String),
+    ByWorld(String),
+}
+
+struct Match {
+    total_kills: u32,
+    players: Vec<String> // ["Dono da bola", "Isgalamido", "Zeh"],
+    kills: HashMap<String, u32>, // "Dono da bola": 5, "Isgalamido": 18, "Zeh": 20 }
+}
+
+impl Match {
+    pub fn add_kill(&mut sef, killer: &str) {
+        //self.kills.
+    }
+
+    pub fn killed_by_world(&mut sef, killed: &str) {
+        //self.kills.
+    }
+
+}
+
+const KILL_REGEX: &str = r#".*\d:\d\d (Kill: \d* \d* \d*): (?P<killer>.*) killed (?P<killed>.*) by (?P<cause>.*)"#;
+
+const INIT_GAME_REGEX: &str = r#".*\d:\d\d (InitGame:)"#;
+
 fn main() {
-    println!("Hello, world!");
+    let kill_regex = Regex::new();
+
+
+    let mut matches: HashMap::new();
+    let path = args_os().nth(1).unwrap_or("res/qgames.log".into());
+    //.expect("provide a path to the file as an argument");
+
+    let path = Path::new(&path);
+    let file = File::open(path).expect("failed to open file");
+    let mapped_data = unsafe { Mmap::map(&file) }.expect("failed to create memory map");
+
+    let raw_data = &*mapped_data;
+
+    let raw_data = raw_data.strip_suffix(b"\n").unwrap_or(raw_data);
+
+    let rows = raw_data
+        //
+        // .par_split(|&b| b == INIT_GAME)
+        .split(|&b| b == b'\n')
+        .map(|row| unsafe { std::str::from_utf8_unchecked(row) })
+        .collect::<Vec<_>>();
+    for row in rows {
+        println!("{row}");
+    }
+
+    // let (city, sample) = row.split_once(|&b| b == b';').expect("no ; separator");
+    // let sample: Value = fast_parse(sample);
+    // (city, sample)
 }
